1e68a02a6b3126c0d06e4bf0ceef45a4ed7646fc
bishnu <bsnadh984@gmail.com>
2022-02-05 16:49:19 +0545

    template literals

---
+++ templateLiterals.js
@@ -0,0 +1,8 @@
+// In ES6 we can use syatax $a(PARAMETER) inside of the back-ticked String.
+
+var myName = "Bishnu Adhikari";
+console.log(`My name is ${myName}`);
+
+// In es5 we had to beak string like below;
+
+var name = 'my name is'+myName;
